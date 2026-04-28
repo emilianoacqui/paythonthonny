ecd240c4ea4d46c6a7fe764944864ec5290f225a
emilianoacqui <emilianoacqui@gmail.com>
2026-04-28 10:46:04 -0300

    ejercicio 4

---
+++ sumasegs.py
@@ -0,0 +1,9 @@
+hh = int(input("ingrese la hora "))
+mm = int(input("ingrese la cantidad de minutos "))
+ss = int(input("ingrese la cantidad de segundos "))
+
+hh = hh * 3600
+mm = mm * 60
+
+suma = hh + mm + ss
+print (suma)
